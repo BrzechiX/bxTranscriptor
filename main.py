99365859c91df294d3da204cb333e6a1586ee0d3
BrzechiX <brzechix@gmail.com>
2023-01-20 01:15:43 +0100

    added option to select audio track, visual fixes

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
 import tkinter as tk
+from tkinter import ttk
 import speech_recognition as sr
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("bxTranscriptor")
-        self.configure(bg="#303030")
-        tk.Button(self, text = "Transcript" , height=1, width=20, bd=0, bg='#2222dd', fg='#ffffff', activebackground='#11ff11', activeforeground="#ffffff", command = self.transcript).grid(padx=20, pady=20)
-
+        self.resizable(False, False)
+        self.name_var = tk.StringVar()
+        self.devicesListbox = ttk.Combobox(self)
+        self.devicesListbox['values'] = sr.Microphone.list_microphone_names()
+        self.devicesListbox['state'] = 'readonly'
+        self.devicesListbox.set('Audio device')
+        self.devicesListbox.grid(row=0, column=0, padx=5, pady=5, columnspan=2, sticky="WENS")
+        ttk.Button(self, text = "Transcript", command = self.transcript).grid(row=0, column=2, padx=5, pady=5, sticky="WENS")
+        self.output = tk.Text()
+        self.output['state'] = 'disabled'
+        self.output.grid(row=1, column=0, padx=5, pady=5, columnspan=3)
+        print(sr.AudioSource)
+        
     def transcript(self):
-        with sr.Microphone() as source:
+        print(sr.Microphone.list_microphone_names().index(self.devicesListbox.get()))
+        with sr.Microphone(device_index=sr.Microphone.list_microphone_names().index(self.devicesListbox.get())) as source:
             try:
                 audio = sr.Recognizer().listen(source)
-                text = sr.Recognizer().recognize_google(audio, language='pl-PL')
-                if text != "":
-                    print(text)
+                self.text = sr.Recognizer().recognize_google(audio, language='pl-PL') # Language here!
+                if self.text != "":
+                    self.output['state'] = 'normal'
+                    self.output.insert('2.0', str(self.text) + "\n\n")
+                    self.output['state'] = 'disabled'
                 else:
                     return 0
             except:
